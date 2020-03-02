f619a05eddcfcb10ab78dcacd41a3de6dd6bcc97
maximusKing2606 <hripamaksa@gmail.com>
2020-03-02 20:11:04 +0200

    Add index.html

---
+++ script.js
@@ -0,0 +1,30 @@
+$(function(){
+
+    
+    let header = $("#header"),
+
+    introH = $("#intro").innerHeight(),
+
+    scrollOffset = $(window).scrollTop();
+
+
+// Fixed Header
+    checkScroll(scrollOffset);
+
+    $(window).on("scroll", function(){
+
+     scrollOffset = $(this).scrollTop();
+
+     checkScroll(scrollOffset);
+});
+
+function checkScroll(scrollOffset) {
+if (scrollOffset >= introH) {
+    header.addClass("fixed");
+} else{
+    header.removeClass("fixed");
+}
+
+}
+
+});
