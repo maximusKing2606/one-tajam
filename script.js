92f5719b512bbe479f7a572cfb39e1834c277be7
maximusKing2606 <hripamaksa@gmail.com>
2020-03-03 20:47:20 +0200

    Some chenges

--- script.js
+++ script.js
@@ -27,4 +27,21 @@ if (scrollOffset >= introH) {
 
 }
 
+// Smoth scroll
+
+$("[data-scroll]").on("click", function(event){
+    event.preventDefault();
+    
+    let $this = $(this),
+     blockId = $this.data('scroll'),
+    blockOffset = $(blockId).offset().top;
+    
+    $("#nav a").removeClass("active");
+    $this.addClass("active");
+    
+    $("html,body").animate({
+        scrollTop: blockOffset
+    },1000)
+    });
+
 });
